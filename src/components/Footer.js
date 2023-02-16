f0fb7f0a3d631e3dae8034e1a738bec446fe711e
Sean Collins <109634748+iseanc@users.noreply.github.com>
2023-02-16 09:29:55 -0700

    added social media links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,6 +4,11 @@ const Footer = () => {
   return (
     <footer className="w-100 mt-auto text-dark p-4">
       <div className="container text-center mb-5">
+        <ul class="social-media">
+          <li><a href="https://www.linkedin.com/in/iseanc">LinkedIn</a></li>
+          <li><a href="https://github.com/iseanc">GitHub</a></li>
+          <li><a href="https://stackoverflow.com/users/19913961/iseanc">StackOverflow</a></li>
+        </ul>
         <h4>&copy; {new Date().getFullYear()} - Sean Collins</h4>
       </div>
     </footer>
